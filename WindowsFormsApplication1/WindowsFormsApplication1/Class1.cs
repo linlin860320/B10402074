174549c695078974c419c5f0fec619fa52b0e5f0
linlin860320 <linlin860320@users.noreply.github.com>
2017-11-19 15:02:55 +0800

    @@
    @@

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        public int GetNumber()
+        {
+            return 0;
+        }
+
+        public int GetNumber2()
+        {
+            return 0;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 g1 = new Class1();
+            label1.Text = g1.GetNumber().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Class1 g2 = new Class1();
+            label2.Text = g2.GetNumber2().ToString();
+        }
+    }
+}
